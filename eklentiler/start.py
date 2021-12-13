411e230c03b4f34c7c41feeba64b6a0b5e578b26
Besoxluk <96026543+Besoxluk@users.noreply.github.com>
2021-12-13 19:42:20 +0300

    Update and rename plugins/start.py to eklentiler/start.py

--- eklentiler/start.py
+++ eklentiler/start.py
@@ -115,7 +115,7 @@ async def start_command(client: Client, message: Message):
         buttons = [
             [InlineKeyboardButton("• ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ •", callback_data="about")],
             [
-                InlineKeyboardButton("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url=client.invitelink),
+                InlineKeyboardButton("Ali", url=client.invitelink),
                 InlineKeyboardButton("𝗚𝗥𝗢𝗨𝗣", url=client.invitelink2),
             ],
             [
